9a18f5b8047c20d872c731344ac5d4aaf5b8a1a7
cbteeple <cbteeple@github.com>
2020-10-07 16:23:03 -0400

    added bounding box and centroid options for center selection

--- examples/build_object_library.py
+++ examples/build_object_library.py
@@ -5,8 +5,8 @@ from object2urdf import ObjectUrdfBuilder
 # Build your library
 object_folder = "ycb"
 builder = ObjectUrdfBuilder(object_folder)
-builder.build_library(force_overwrite=True, decompose_concave=True, force_decompose=False, calculate_mass_center = True)
+builder.build_library(force_overwrite=True, decompose_concave=True, force_decompose=False, center = 'top')
 
 object_folder = "fancy_cube"
 builder = ObjectUrdfBuilder(object_folder)
-builder.build_library(force_overwrite=True, decompose_concave=True, force_decompose=False, calculate_mass_center = True)
+builder.build_library(force_overwrite=True, decompose_concave=True, force_decompose=False, center = 'mass')
