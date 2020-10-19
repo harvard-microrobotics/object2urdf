842d490c9c90ad8f6bf861b624f7739375a4d3db
cbteeple <cbteeple@github.com>
2020-10-19 14:37:01 -0400

    fixed COM issue when scene is imported

--- examples/build_object_library.py
+++ examples/build_object_library.py
@@ -3,9 +3,13 @@ import sys
 from object2urdf import ObjectUrdfBuilder
 
 # Build your library
+object_folder = "basketball"
+builder = ObjectUrdfBuilder(object_folder)
+builder.build_library(force_overwrite=True, decompose_concave=True, force_decompose=False, center = 'bottom')
+
 object_folder = "ycb"
 builder = ObjectUrdfBuilder(object_folder)
-builder.build_library(force_overwrite=True, decompose_concave=True, force_decompose=True, center = 'top')
+builder.build_library(force_overwrite=True, decompose_concave=True, force_decompose=False, center = 'top')
 
 object_folder = "fancy_cube"
 builder = ObjectUrdfBuilder(object_folder)
