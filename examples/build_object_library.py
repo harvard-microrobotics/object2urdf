d293d84153450aa7fcdd0e1a4845c77eb81a2c38
cbteeple <cbteeple@github.com>
2020-10-20 11:46:03 -0400

    added hoops with no net

--- examples/build_object_library.py
+++ examples/build_object_library.py
@@ -4,11 +4,18 @@ from object2urdf import ObjectUrdfBuilder
 
 # Build single URDFs
 object_folder = "basketball"
+
+builder = ObjectUrdfBuilder(object_folder, urdf_prototype='_prototype_ball.urdf')
+builder.build_urdf(filename="basketball/ball/basketball_corrected.obj", force_overwrite=True, decompose_concave=True, force_decompose=False, center = 'mass')
+
 builder = ObjectUrdfBuilder(object_folder, urdf_prototype='_prototype_hoop.urdf')
 builder.build_urdf(filename="basketball/hoop/basketball_net_and_board.obj", force_overwrite=True, decompose_concave=True, force_decompose=False, center = 'bottom')
 
-builder = ObjectUrdfBuilder(object_folder, urdf_prototype='_prototype_ball.urdf')
-builder.build_urdf(filename="basketball/ball/basketball_corrected.obj", force_overwrite=True, decompose_concave=True, force_decompose=False, center = 'bottom')
+builder = ObjectUrdfBuilder(object_folder, urdf_prototype='_prototype_hoop.urdf')
+builder.build_urdf(filename="basketball/hoop_no_net/basketball_net_and_board.obj", force_overwrite=True, decompose_concave=True, force_decompose=False, center = 'bottom')
+
+builder = ObjectUrdfBuilder(object_folder, urdf_prototype='_prototype_hoop.urdf')
+builder.build_urdf(filename="basketball/hoop_no_net_simple/basketball_net_and_board.obj", force_overwrite=True, decompose_concave=True, force_decompose=False, center = 'bottom')
 
 # Build entire libraries of URDFs
 object_folder = "ycb"
